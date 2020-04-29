3bd72e7d7f3b1018b164e86924d99e8ee239f879
Edith <edith.andrews@gmail.com>
2020-04-29 07:42:41 +0000

    Add a Static Pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
-    render inline: "hi junaid i am scare this is all looking v <em>complique</em>!"
+     render html: "sup"
   end
 end
